b575dbf91253842b7a6dd1c63ddc8553059e38ab
checktheroads <matt@stellar.tech>
2019-10-27 02:43:53 -0700

    debug

--- NetlifyFunctions/contactform.js
+++ NetlifyFunctions/contactform.js
@@ -2,35 +2,45 @@ let request = require("request");
 
 // Handle the lambda invocation
 exports.handler = function(event, context, callback) {
-    const data = event.body;
-    const names = data.contactName.split(" ");
-    const apiURL = "https://webhook.site/d2d2dbd7-ba82-4ca9-8c19-85b90927156b";
-    const formData = {
-        Company: data.contactCompany,
-        LastName: names[1] || "",
-        FirstName: names[0] || "",
-        Email: data.contactEmail,
-        Phone: data.contactPhone,
-        LeadSource: "stellar.tech Contact Form",
-        Description: `Form Data:\nSubject:${data.contactSubject}\nMessage:\n${
-            data.contactMessage
-        }\n\nMetadata:\n${""}`
-    };
-    request.post({ url: apiURL, json: formData }, function(
-        err,
-        httpResponse,
-        body
-    ) {
-        let msg;
-        if (err) {
-            msg = `Error submitting form: ${err}`;
-        } else {
-            msg = `Submitted form: ${JSON.stringify(body)}`;
-        }
-        callback(null, {
-            statusCode: 200,
-            body: msg
+    console.info(event);
+    try {
+        const data = event.body;
+        const names = data.contactName.split(" ");
+        const apiURL =
+            "https://webhook.site/d2d2dbd7-ba82-4ca9-8c19-85b90927156b";
+        const formData = {
+            Company: data.contactCompany,
+            LastName: names[1] || "",
+            FirstName: names[0] || "",
+            Email: data.contactEmail,
+            Phone: data.contactPhone,
+            LeadSource: "stellar.tech Contact Form",
+            Description: `Form Data:\nSubject:${
+                data.contactSubject
+            }\nMessage:\n${data.contactMessage}\n\nMetadata:\n${""}`
+        };
+        request.post({ url: apiURL, json: formData }, function(
+            err,
+            httpResponse,
+            body
+        ) {
+            let msg;
+            if (err) {
+                msg = `Error submitting form: ${err}`;
+            } else {
+                msg = `Submitted form: ${JSON.stringify(body)}`;
+            }
+            callback(null, {
+                statusCode: 200,
+                body: msg
+            });
+            console.log(`Message: ${msg}`);
         });
-        console.log(`Message: ${msg}`);
-    });
+    } catch (error) {
+        console.error(error);
+        callback(null, {
+            statusCode: 500,
+            body: error
+        });
+    }
 };
