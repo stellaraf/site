6160269a3dddd5f868ad6eb5dc0237e1ac363661
Matt Love <mtt.lov@gmail.com>
2019-10-26 21:33:14 -0700

    Add form context

--- NetlifyFunctions/contactform.js
+++ NetlifyFunctions/contactform.js
@@ -23,7 +23,8 @@ exports.handler = async (event, context, callback) => {
         FirstName: names[0],
         Email: data.contactEmail,
         Phone: data.contactPhone,
-        LeadSource: "stellar.tech Contact Form"
+        LeadSource: "stellar.tech Contact Form",
+        Description: `Form Data:\nSubject:${data.contactSubject}\nMessage:\n${data.contactMessage}\n\nMetadata:\n${}`
     };
     const apiConfig = {
         url: "https://webhook.site/d2d2dbd7-ba82-4ca9-8c19-85b90927156b",
