ea7ee429ce32e33b4b459d31cc38a8c61fdbbb36
checktheroads <matt@stellar.tech>
2019-10-28 00:20:37 -0700

    debug

--- NetlifyFunctions/contactform.js
+++ NetlifyFunctions/contactform.js
@@ -15,8 +15,7 @@ exports.handler = (event, context, callback) => {
     try {
         const data = event.body;
         console.info(`[contactform.js]: event.body: ${event.body}`);
-        console.info(`[contactform.js]: data string: ${JSON.stringify(data)}`);
-        console.info(`[contactform.js]: data obj: ${data}`);
+        console.info(`[contactform.js]: data type: ${typeof data}`);
         console.info(`[contactform.js]: data.contactName: ${data.contactName}`);
         console.info(
             `[contactform.js]: event.body.contactName: ${event.body.contactName}`
