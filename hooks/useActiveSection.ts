1105a1a1e49df9871b2941c124b35e5103b9c7a2
checktheroads <matt@allroads.io>
2020-08-15 00:37:15 -0700

    fix over-rendering due to scroll tracking

--- hooks/useActiveSection.ts
+++ hooks/useActiveSection.ts
@@ -1,7 +1,8 @@
-import { MutableRefObject } from 'react';
-import { BoxProps } from '@chakra-ui/core';
+import { useState } from 'react';
 import { useScrollPosition } from '@n8tb1t/use-scroll-position';
 import { isEqual } from 'lodash';
+import type { MutableRefObject } from 'react';
+import type { BoxProps } from '@chakra-ui/core';
 
 type Sections = [MutableRefObject<HTMLElement>, BoxProps];
 
@@ -15,31 +16,40 @@ export const useActiveSection = (
   [...deps]: any,
   [...sections]: Sections[],
 ) => {
+  const first = sections[0];
   const last = sections.length - 1;
-
+  const [style, setStyle] = useState(defaultState);
+  const [sameAsCurrent, setSameAsCurrent] = useState(true);
+  const handleChange = newState => {
+    const isSame = isEqual(currentState, newState);
+    if (isSame !== sameAsCurrent) {
+      setSameAsCurrent(isSame);
+    }
+    setStyle(newState);
+  };
   const effect = ({ currPos }) => {
     const { y } = currPos;
-    let toSet = defaultState;
-    let sameAsCurrent = false;
+
     for (const [i, section] of sections.entries()) {
       const [ref, stateObj] = section;
       if (i === last) {
         if (y < -ref.current.offsetTop) {
-          sameAsCurrent = isEqual(currentState, stateObj);
-          toSet = stateObj;
+          handleChange(stateObj);
         }
       } else {
         const [nextRef] = sections[i + 1];
         if (y >= -nextRef.current.offsetTop && y <= -ref.current.offsetTop) {
-          sameAsCurrent = isEqual(currentState, stateObj);
-          toSet = stateObj;
+          handleChange(stateObj);
         }
       }
     }
+    if (y > -first[0].current.offsetTop) {
+      setTimeout(() => handleChange(defaultState), 250);
+    }
     if (!sameAsCurrent) {
-      callback(toSet);
+      callback(style);
     }
   };
 
-  return useScrollPosition(effect, deps);
+  return useScrollPosition(effect, [deps]);
 };
