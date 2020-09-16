62850f4e1be2292c766f55f0adf1acb130e5903f
checktheroads <matt@allroads.io>
2020-09-16 10:43:42 -0700

    migrate CallToAction to TS directory structure

--- components/CallToAction/CallToAction.tsx
+++ components/CallToAction/CallToAction.tsx
@@ -1,17 +1,18 @@
 import * as React from 'react';
 import { Box, Flex, Heading, useMultiStyleConfig } from '@chakra-ui/core';
-import type { BoxProps } from '@chakra-ui/core';
 
-export const CalltoAction = (props: BoxProps) => {
+import type { CTAProps } from './types';
+
+export const CalltoAction = (props: CTAProps) => {
   const styles = useMultiStyleConfig('SyncedStyles', { variant: 0 });
   return (
     <Box
-      className="__actions"
       py={24}
       px={24}
+      sx={styles.box}
+      className="__actions"
       borderBottomWidth="1px"
       borderBottomColor="original.tertiary"
-      sx={styles.box}
       {...props}>
       <Flex justifyContent="space-between">
         <Heading as="h5" fontSize="2xl">
