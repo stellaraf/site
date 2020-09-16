624e7fd84c734b6844323640e617817374494f19
checktheroads <matt@allroads.io>
2020-09-16 10:16:52 -0700

    migrate Carousel to TS directory structure

--- components/Carousel/Carousel.tsx
+++ components/Carousel/Carousel.tsx
@@ -3,21 +3,7 @@ import { createContext, useContext, useState } from 'react';
 import { Box, Button, Flex } from '@chakra-ui/core';
 import { useTransition, animated } from 'react-spring';
 
-interface CarouselProps {
-  interval?: number;
-  noDots?: boolean;
-  dotColor?: string;
-  children: React.ReactNodeArray;
-  [k: string]: any;
-}
-
-type SetCurrent = (c: number) => void;
-
-interface UseCarousel {
-  total: number;
-  current: number;
-  setCurrent: SetCurrent;
-}
+import type { DotProps, CarouselProps, SetCurrent, UseCarousel } from './types';
 
 /**
  * Cycle through range of numbers `total` starting with `current`.
@@ -41,23 +27,28 @@ const useCarousel = (): UseCarousel => useContext(CarouselContext);
 /**
  * Show a row of dots for each Hero Card. The currently active card's dot is filled.
  */
-const Dots = ({ color }) => {
+const Dots = (props: DotProps) => {
+  const { color, ...rest } = props;
   const { current, setCurrent, total } = useCarousel();
   return (
     <Flex position="absolute" width="100%" bottom={0} alignItems="flex-end" justifyContent="center">
-      {Array.apply(null, Array(total)).map((_, i) => (
-        <Button key={i} mx={1} minW="unset" variant="unstyled" onClick={() => setCurrent(i)}>
-          <Box
-            width={2}
-            height={2}
-            borderWidth="1px"
-            borderRadius="50%"
-            borderStyle="solid"
-            borderColor={color}
-            bg={i === current ? color : 'transparent'}
-          />
-        </Button>
-      ))}
+      {Array.apply(null, Array(total)).map((_, i) => {
+        const handleClick = (): void => setCurrent(i);
+        return (
+          <Button key={i} mx={1} minW="unset" variant="unstyled" onClick={handleClick}>
+            <Box
+              width={2}
+              height={2}
+              borderWidth="1px"
+              borderRadius="50%"
+              borderStyle="solid"
+              borderColor={color}
+              bg={i === current ? color : 'transparent'}
+              {...rest}
+            />
+          </Button>
+        );
+      })}
     </Flex>
   );
 };
@@ -65,13 +56,8 @@ const Dots = ({ color }) => {
 /**
  * Cycle through child elements with animation.
  */
-export const Carousel = ({
-  interval = 4000,
-  noDots = false,
-  dotColor = 'black',
-  children,
-  ...props
-}: CarouselProps) => {
+export const Carousel = (props: CarouselProps) => {
+  const { interval = 4000, noDots = false, dotColor = 'black', children, ...rest } = props;
   const [current, setCurrent] = useState(0);
   const transition = useTransition(children[current], {
     from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
@@ -81,7 +67,7 @@ export const Carousel = ({
   });
   return (
     <CarouselContext.Provider value={{ current, setCurrent, transition, total: children.length }}>
-      <Box width="100%" height="100%" position="relative" {...props}>
+      <Box width="100%" height="100%" position="relative" {...rest}>
         <Box width="100%" height="100%" position="absolute">
           {transition((style, item) => (
             <animated.div style={style}>{item}</animated.div>
