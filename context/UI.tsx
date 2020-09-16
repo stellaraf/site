1bf1946444607645f1057b57fc1890b71973e22b
checktheroads <matt@allroads.io>
2020-09-16 16:56:24 -0700

    migrate context types to types directory

--- context/UI.tsx
+++ context/UI.tsx
@@ -1,24 +1,19 @@
 import * as React from 'react';
+import { useMemo } from 'react';
 import { ChakraProvider, useTheme as useChakraTheme } from '@chakra-ui/core';
+import { makeTheme } from 'site/util';
 
-import type { ReactNode } from 'react';
-import type { CustomTheme } from 'site/types';
+import type { UseTheme, UIProviderProps } from 'site/types';
 
-type UseTheme = () => CustomTheme;
-
-interface UIProviderProps {
-  theme: CustomTheme;
-  children?: ReactNode;
-}
-
-export const UIProvider = ({ theme, children }: UIProviderProps) => {
+export const UIProvider = (props: UIProviderProps) => {
+  const { theme, children } = props;
+  const generatedTheme = useMemo(() => makeTheme(theme), [theme]);
   return (
-    <ChakraProvider theme={theme} resetCSS>
+    <ChakraProvider resetCSS theme={generatedTheme}>
       {children}
     </ChakraProvider>
   );
 };
-
 export const useTheme: UseTheme = useChakraTheme;
 
 export { useColorMode } from '@chakra-ui/core';
--- context/index.tsx
+++ context/index.tsx
@@ -2,23 +2,21 @@ import * as React from 'react';
 import { UIProvider } from './UI';
 import { ConfigProvider } from './Config';
 import { MediaProvider } from './Media';
-import type { GlobalConfig, CustomTheme } from 'site/types';
+import type { ProviderProps } from 'site/types';
 
 export { useMedia } from './Media';
 export { useConfig } from './Config';
 export { useTheme } from './UI';
 export { useColorMode, useColorValue } from './UI';
 
-interface ProviderProps {
-  globalConfig: GlobalConfig;
-  theme: CustomTheme;
-  children: any;
-}
-
-export const Provider = ({ globalConfig, theme, children }: ProviderProps) => (
-  <ConfigProvider globalConfig={globalConfig}>
-    <UIProvider theme={theme}>
-      <MediaProvider>{children}</MediaProvider>
-    </UIProvider>
-  </ConfigProvider>
-);
+export const Provider = (props: ProviderProps) => {
+  const { children, appConfig } = props;
+  const { theme, ...globalConfig } = appConfig;
+  return (
+    <ConfigProvider globalConfig={globalConfig}>
+      <UIProvider theme={theme}>
+        <MediaProvider>{children}</MediaProvider>
+      </UIProvider>
+    </ConfigProvider>
+  );
+};
