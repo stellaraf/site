36215633b691265fbfd6142b8917027305dc537b
checktheroads <matt@allroads.io>
2020-09-21 14:15:36 -0700

    migrate chakra-ui style hooks to separate files

---
+++ hooks/useGradient.ts
@@ -0,0 +1,11 @@
+import { useColorValue } from 'site/context';
+
+import type { BoxProps } from '@chakra-ui/core';
+
+const DARK = {
+  background:
+    'linear-gradient(180deg, rgba(43, 60, 143, 1) 0%, rgba(42, 23, 74, 1) 50%, rgba(22, 19, 24, 1) 100%)',
+};
+const LIGHT = { backgroundColor: 'original.light' };
+
+export const useGradient = (): BoxProps => useColorValue(LIGHT, DARK);
---
+++ styles/heroButtons.ts
@@ -0,0 +1,37 @@
+import { mode } from '@chakra-ui/theme-tools';
+
+import type { Dict } from 'site/types';
+
+export const heroButtons = {
+  variants: {
+    heroPrimary: (props: Dict) =>
+      mode(
+        {
+          bg: 'primary.500',
+          color: 'white',
+          boxShadow: 'md',
+          mx: 4,
+          _hover: { bg: 'primary.400' },
+        },
+        {
+          bg: 'teal.400',
+          color: 'white',
+          boxShadow: 'md',
+          mx: 4,
+          _hover: { bg: 'teal.300', color: 'white' },
+        },
+      )(props),
+    heroSecondary: (props: Dict) =>
+      mode(
+        { bg: 'light.50', color: 'black', boxShadow: 'md', mx: 4, _hover: { bg: 'blackAlpha.50' } },
+        {
+          bg: 'transparent',
+          borderColor: 'white',
+          borderWidth: '1px',
+          boxShadow: 'md',
+          mx: 4,
+          _hover: { bg: 'whiteAlpha.100' },
+        },
+      )(props),
+  },
+};
