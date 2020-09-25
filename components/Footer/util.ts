f55a9e22db8b8ec8b2e7d7a715aa38a463c7d12b
checktheroads <matt@allroads.io>
2020-09-24 17:40:50 -0700

    fix SSR fetch error, fix submission message sync

--- components/Footer/util.ts
+++ components/Footer/util.ts
@@ -5,7 +5,11 @@ export async function subscribeEmail(email: string) {
   const url = process.env.NEXT_PUBLIC_SUBSCRIBE1_URL ?? '';
   const headerName = process.env.NEXT_PUBLIC_SUBSCRIBE1_HEADER ?? '';
   const key = process.env.NEXT_PUBLIC_SUBSCRIBE1_KEY ?? '';
-  return await post(url, { email }, { headers: { [headerName]: key } });
+  let response;
+  if (typeof window !== 'undefined') {
+    response = await post(url, { email }, { headers: { [headerName]: key } });
+  }
+  return response;
 }
 
 export const subscribeSchema = yup.object().shape({
