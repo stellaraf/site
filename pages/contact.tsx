60213b464f306495b2d009da0a8e02236fffc381
checktheroads <matt@allroads.io>
2020-10-15 17:05:37 -0700

    add phone number to contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import { Box, Flex } from '@chakra-ui/core';
+import { Box, Flex, Button as ChakraButton, Heading, VStack } from '@chakra-ui/core';
+import { ImPhone as Phone } from '@meronex/icons/im';
 import { getPage, getPageContent, getContent } from 'site/util';
-import { Hero, Options, SEO } from 'site/components';
-import { useRender } from 'site/hooks';
+import { Content, Hero, Options, SEO } from 'site/components';
+import { useRender, useSlug } from 'site/hooks';
 import { useResponsiveStyle } from 'site/styles';
 
 import type { IContactPage, GetStaticProps } from 'site/types';
@@ -10,21 +11,45 @@ import type { IContactPage, GetStaticProps } from 'site/types';
 const SLUG = 'contact';
 
 export default function Contact(props: IContactPage) {
-  const { pageData, contactCards } = props;
+  const { pageData, contactCards, pageContent } = props;
   const cards = contactCards.sort((a, b) => a.sortWeight - b.sortWeight);
   const { title, subtitle, body, customProperties } = pageData;
   const renderedBody = useRender(body);
   const rStyles = useResponsiveStyle();
+  const content = pageContent[0];
+  const slug = useSlug(content.title, [content.title]);
   return (
     <>
       <SEO title={customProperties?.metaTitle ?? title} description={subtitle} />
-      <Hero title={title} subtitle={subtitle} body={renderedBody} minH="80vh">
+      <Hero title={title} subtitle={subtitle} body={renderedBody} minH="40vh">
         <Box as="section" py={{ base: 16, lg: 32 }}>
           <Flex height="100%" align="center" direction="column" {...rStyles}>
             <Options cards={cards} />
           </Flex>
         </Box>
       </Hero>
+      <Box as="section" overflow="hidden">
+        <Flex height="100%" overflow="hidden" align="center" direction="column" {...rStyles}>
+          <VStack spacing={4}>
+            <Content.Title id={slug}>{content.title}</Content.Title>
+            {content.subtitle && <Content.Subtitle>{content.subtitle}</Content.Subtitle>}
+          </VStack>
+
+          <VStack spacing={4} my={12}>
+            <ChakraButton
+              as="a"
+              boxSize="3rem"
+              href={content.buttonLink}
+              rounded="full"
+              colorScheme="green">
+              <Phone />
+            </ChakraButton>
+            <Heading as="h4" fontSize="xl" fontWeight="medium">
+              {content.buttonText}
+            </Heading>
+          </VStack>
+        </Flex>
+      </Box>
     </>
   );
 }
