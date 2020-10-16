89ca1d58463629032d35cb5a83eabce7ec0ecea0
checktheroads <matt@allroads.io>
2020-10-16 16:53:55 -0700

    fix left margin on lists

--- components/RichText/Lists.tsx
+++ components/RichText/Lists.tsx
@@ -3,8 +3,8 @@ import { List, ListItem } from '@chakra-ui/core';
 
 import type { UlProps, OlProps, LiProps } from './types';
 
-export const Ul = (props: UlProps) => <List styleType="circle" {...props} />;
+export const Ul = (props: UlProps) => <List styleType="circle" ml={8} {...props} />;
 
-export const Ol = (props: OlProps) => <List as="ol" styleType="decimal" {...props} />;
+export const Ol = (props: OlProps) => <List as="ol" styleType="decimal" ml={8} {...props} />;
 
 export const Li = (props: LiProps) => <ListItem {...props} />;
