fa4d5f6c79b59b11b2ba2629f67834d3226f1a3e
checktheroads <matt@allroads.io>
2020-10-19 16:50:04 -0700

    migrate custom Icon to global component

---
+++ components/Icon/Icon.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { Box, Flex, Icon as ChakraIcon, Image } from '@chakra-ui/core';
+import { useOpposingColor } from 'site/hooks';
+import { useColorValue } from 'site/context';
+import type { IIcon } from './types';
+
+export const Icon = (props: IIcon) => {
+  const { color: bgColor = 'primary', icon, size = 20, ...rest } = props;
+  const bg = useColorValue(`original.${bgColor}`, `${bgColor}.300`);
+  const color = useOpposingColor(bg);
+  let component = null;
+  if (typeof icon === 'string') {
+    component = (
+      <Box css={{ mask: `url(${icon}) no-repeat center` }} backgroundColor={color} boxSize="50%" />
+    );
+  } else if (typeof icon === 'undefined') {
+    component = <ChakraIcon boxSize="100%" />;
+  } else {
+    component = <ChakraIcon as={icon} boxSize="100%" />;
+  }
+
+  return (
+    <Flex
+      bg={bg}
+      minWidth={size}
+      height={size}
+      align="center"
+      justify="center"
+      borderRadius="full"
+      color={color}
+      overflow="hidden"
+      {...rest}>
+      {component}
+    </Flex>
+  );
+};
