492dd8c305f65aa101a7332cbcd1eceeb3f858cd
checktheroads <matt@allroads.io>
2020-10-27 10:21:15 -0700

    render body in Hero component, not on page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,24 +1,13 @@
-import * as React from 'react';
 import { Box, Flex, Heading } from '@chakra-ui/core';
-import { getPage, getPageContent, getBios } from 'site/util';
+import { getPage, getPageContent, getContent } from 'site/util';
 import { Avatars, Hero, GoogleMap, SEO } from 'site/components';
-import { useRender, useRef, useTitle } from 'site/hooks';
+import { useRef, useTitle } from 'site/hooks';
 import { useResponsiveStyle } from 'site/styles';
 
 import type { GetStaticProps } from 'next';
-import type { IAboutPage, BioEntry, Bio, ISection } from 'site/types';
+import type { IAboutPage, ISection } from 'site/types';
 
 const SLUG = 'about';
-
-/**
- * Extract relevant fields from Contentful response.
- */
-function* parseBios(raw: BioEntry): Generator<Bio> {
-  for (let b of raw.fields.bios) {
-    const { photo, ...rest } = b.fields;
-    yield { ...rest, photo: photo.fields.file };
-  }
-}
 
 const Section = (props: ISection) => {
   const { title, children, ...rest } = props;
@@ -37,19 +26,17 @@ const Section = (props: ISection) => {
 
 export default function About(props: IAboutPage) {
   const { pageData, bios } = props;
-  const { title, subtitle, body, customProperties } = pageData;
+  const { title, subtitle, body = null, customProperties } = pageData;
   const { employeesTitle = '', mapTitle = '' } = customProperties;
   const headerRef = useRef<HTMLDivElement>();
-  const parsedBios = [...parseBios(bios)];
-  const renderedBody = useRender(body);
   const titleMe = useTitle();
 
   return (
     <>
       <SEO title={title} description={subtitle} />
-      <Hero ref={headerRef} title={title} subtitle={subtitle} body={renderedBody} />
+      <Hero ref={headerRef} title={title} subtitle={subtitle} body={body} />
       <Section title={titleMe(employeesTitle)}>
-        <Avatars bios={parsedBios} />
+        <Avatars bios={bios} />
       </Section>
       <Section title={titleMe(mapTitle)}>
         <GoogleMap />
@@ -58,14 +45,14 @@ export default function About(props: IAboutPage) {
   );
 }
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getStaticProps: GetStaticProps<IAboutPage> = async () => {
   let pageData = Object();
   let pageContent = Array();
-  let bios = Object();
+  let bios = new Array();
   try {
     pageData = await getPage(SLUG);
     pageContent = await getPageContent(pageData?.id ?? null);
-    bios = await getBios();
+    bios = await getContent('bio');
   } catch (err) {
     console.error(err);
     throw err;
