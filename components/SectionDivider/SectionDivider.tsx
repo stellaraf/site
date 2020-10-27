331ce640e67867c5d31b919b08e7790d3aa53c6f
checktheroads <matt@allroads.io>
2020-10-27 15:15:46 -0700

    slightly improve section divider style

--- components/SectionDivider/SectionDivider.tsx
+++ components/SectionDivider/SectionDivider.tsx
@@ -1,30 +1,28 @@
-import * as React from 'react';
-import { Box } from '@chakra-ui/core';
-import { useTheme } from 'site/context';
+import { Box, useToken } from '@chakra-ui/core';
 
 import type { ISectionDivider } from './types';
 
 export const SectionDivider = (props: ISectionDivider) => {
   const { left = true, right = false, straight = false, ...rest } = props;
-  const { colors } = useTheme();
-  const { primary, secondary, tertiary } = colors.original;
   let transform = Object();
-  if (!straight && left) {
-    transform = { transform: 'rotate(-3deg)' };
-  } else if (!straight && right) {
+  if (!straight && right) {
     transform = { transform: 'rotate(3deg)' };
+  } else if (!straight && left) {
+    transform = { transform: 'rotate(-3deg)' };
   }
+  const one = useToken('colors', 'tertiary.600');
+  const two = useToken('colors', 'tertiary.700');
+  const three = useToken('colors', 'tertiary.800');
   return (
     <Box
       pos="relative"
       height="1px"
-      width="100%"
+      opacity={0.9}
+      width="150%"
       overflow="hidden"
-      bg="tertiary.200"
+      bg="original.tertiary"
       {...transform}
-      // transform="rotate(-3deg)"
-      // boxShadow="0 0 15px #4CC9F0, 0 0 25px #2b3c8f, 0 0 35px #2a174a"
-      boxShadow={`0 0 15px ${tertiary}, 0 0 25px ${primary}, 0 0 35px ${secondary}`}
+      boxShadow={`0 0 10px 1px ${one}, 0 0 20px 2px ${two}, 0 0 30px 4px ${three}`}
       {...rest}
     />
   );
