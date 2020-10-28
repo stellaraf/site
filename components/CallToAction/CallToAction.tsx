8c2c72216dd7a31c75f90a1841fa04dcb25bdcba
checktheroads <matt@allroads.io>
2020-10-28 16:47:27 -0700

    animate call to action entrance when visible, don't animate on color mode change

--- components/CallToAction/CallToAction.tsx
+++ components/CallToAction/CallToAction.tsx
@@ -1,5 +1,7 @@
+import { memo } from 'react';
 import { useRouter } from 'next/router';
 import { Center, Heading, VStack, Wrap } from '@chakra-ui/core';
+import { useInView } from 'react-intersection-observer';
 import { AnimatedBox, SectionDivider } from 'site/components';
 import { useConfig, useColorValue } from 'site/context';
 import { useTitle } from 'site/hooks';
@@ -7,13 +9,11 @@ import { useResponsiveStyle } from 'site/styles';
 import { Action } from './Action';
 import { filterActions, randomActions } from './util';
 
-import type { ICallToAction } from './types';
+import type { ICallToAction, ICallToActionMemo } from './types';
 
-export const CallToAction = (props: ICallToAction) => {
-  const { actions: rawActions, ...rest } = props;
-  const { asPath: currentPath } = useRouter();
+export const _CallToActionContainer = (props: ICallToActionMemo) => {
+  const { actions: rawActions, currentPath, ...rest } = props;
 
-  const showBorder = useColorValue(false, true);
   const rStyles = useResponsiveStyle();
   const titleMe = useTitle();
 
@@ -26,6 +26,8 @@ export const CallToAction = (props: ICallToAction) => {
   // Randomize & limit the actions shown.
   const actions = randomActions(callsToActionShown, filteredActions);
 
+  const [ref, inView] = useInView({ triggerOnce: true, rootMargin: '-100px' });
+
   return (
     <>
       <VStack py={24} spacing={12} className="__actions" minH="30vh" {...rStyles} {...rest}>
@@ -34,22 +36,43 @@ export const CallToAction = (props: ICallToAction) => {
             {titleMe(callToActionTitle)}
           </Heading>
         </Center>
-        <Wrap direction={{ base: 'column', lg: 'row' }} spacing={8} justify="center">
-          {actions.map((action, i) => (
-            <AnimatedBox
-              zIndex={1}
-              animate={{ x: 0 }}
-              key={`action${i}`}
-              initial={{ x: '100%' }}
-              whileTap={{ y: '-3%' }}
-              whileHover={{ y: '-5%' }}
-              transition={{ delay: i * 0.1 }}>
-              <Action {...action} />
-            </AnimatedBox>
-          ))}
+        <Wrap direction={{ base: 'column', lg: 'row' }} spacing={8} justify="center" ref={ref}>
+          {inView &&
+            actions.map((action, i) => (
+              <AnimatedBox
+                zIndex={1}
+                animate={{ x: 0 }}
+                key={`action${i}`}
+                initial={{ x: '100%' }}
+                whileTap={{ y: '-3%' }}
+                whileHover={{ y: '-5%' }}
+                transition={{ delay: i * 0.1 }}>
+                <Action {...action} />
+              </AnimatedBox>
+            ))}
         </Wrap>
       </VStack>
-      {showBorder && <SectionDivider straight />}
+    </>
+  );
+};
+
+/**
+ * Memoize the CTA container so that it doesn't re-render on every color-mode change. Because the
+ * visibility of the <SectionDivider /> is dependent on dark-mode, this is expected, but a little
+ * annoying.
+ */
+const CallToActionContainer = memo(
+  _CallToActionContainer,
+  (prev, next) => prev.currentPath === next.currentPath,
+);
+
+export const CallToAction = (props: ICallToAction) => {
+  const showBorder = useColorValue(0, 1);
+  const { asPath: currentPath } = useRouter();
+  return (
+    <>
+      <CallToActionContainer currentPath={currentPath} {...props} />
+      <SectionDivider straight opacity={showBorder} />
     </>
   );
 };
--- components/CallToAction/types.ts
+++ components/CallToAction/types.ts
@@ -4,4 +4,8 @@ import type { IActions } from 'site/types';
 export interface ICallToAction extends BoxProps {
   actions: IActions[];
 }
+export interface ICallToActionMemo extends ICallToAction {
+  currentPath: string;
+}
+
 export type { IActions } from 'site/types';
