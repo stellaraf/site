e0e41b610e0bb515afdd707945e8af763ee67a1e
checktheroads <matt@allroads.io>
2020-10-28 16:49:17 -0700

    animate visibility of content images

--- components/Content/Image.tsx
+++ components/Content/Image.tsx
@@ -1,12 +1,18 @@
-import * as React from 'react';
 import { Box, Image as ChakraImage } from '@chakra-ui/core';
+import { useInView } from 'react-intersection-observer';
 
 import type { IImage } from './types';
 
 export const Image = (props: IImage) => {
   const { src, boxSize = 'xl', ...rest } = props;
+  const [ref, inView] = useInView({ triggerOnce: true, rootMargin: '-150px' });
   return (
-    <Box boxSize={boxSize} {...rest}>
+    <Box
+      ref={ref}
+      opacity={+inView}
+      transition="opacity 0.2s ease-in-out"
+      boxSize={boxSize}
+      {...rest}>
       <ChakraImage src={src} boxSize={boxSize} />
     </Box>
   );
