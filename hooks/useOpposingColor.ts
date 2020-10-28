35613024771540a064c4c019d9d98aad543bef3e
checktheroads <matt@allroads.io>
2020-10-27 17:15:58 -0700

    extend opposing color hooks

--- hooks/useOpposingColor.ts
+++ hooks/useOpposingColor.ts
@@ -1,29 +1,45 @@
 import { useState } from 'react';
+import { useToken } from '@chakra-ui/core';
 import { getColor, isLight } from '@chakra-ui/theme-tools';
 import { useTheme } from 'site/context';
 
 interface IOpposingOptions {
-  light: string;
-  dark: string;
+  light?: string;
+  dark?: string;
 }
 
-export function useOpposingColor(color: string, options?: IOpposingOptions): string {
+export function useIsDark(color: string) {
   const theme = useTheme();
-  const dark = options?.dark ?? theme.colors.original.dark;
-  const light = options?.light ?? theme.colors.original.light;
-
   if (typeof color === 'string' && color.match(/[a-zA-Z]+\.[a-zA-Z0-9]+/g)) {
     color = getColor(theme, color, color);
   }
-
-  let isBlack = true;
+  let opposingShouldBeDark = true;
   try {
-    isBlack = isLight(color)(theme);
+    opposingShouldBeDark = isLight(color)(theme);
   } catch (err) {
     console.error(err);
   }
+  return opposingShouldBeDark;
+}
 
+export function useOpposingColor(color: string, options?: IOpposingOptions): string {
   const [opposingColor, setOpposingColor] = useState<string>('inherit');
+  const isBlack = useIsDark(color);
+
+  const dark = useToken('colors', options?.dark ?? 'original.dark');
+  const light = useToken('colors', options?.light ?? 'original.light');
+
+  isBlack && opposingColor !== dark && setOpposingColor(dark);
+  !isBlack && opposingColor !== light && setOpposingColor(light);
+
+  return opposingColor;
+}
+
+export function useOpposingToken(color: string, options?: IOpposingOptions): string {
+  const [opposingColor, setOpposingColor] = useState<string>('inherit');
+  const isBlack = useIsDark(color);
+  const dark = options?.dark ?? 'dark';
+  const light = options?.light ?? 'light';
 
   isBlack && opposingColor !== dark && setOpposingColor(dark);
   !isBlack && opposingColor !== light && setOpposingColor(light);
