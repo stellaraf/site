7be8421765c16454ee52637f3a62030f21971f1e
checktheroads <matt@allroads.io>
2020-10-28 16:55:43 -0700

    animate home page image presence

--- components/HomeSection/HomeBlock.tsx
+++ components/HomeSection/HomeBlock.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
 import { Box, Center, Grid, Image, Heading } from '@chakra-ui/core';
+import { useInView } from 'react-intersection-observer';
 import { Button } from 'site/components';
 import type { IHomeBlock } from './types';
 
@@ -18,7 +18,7 @@ const columns = { right: '0.5fr 1fr', left: '1fr 0.5fr' };
 
 export const HomeBlock = (props: IHomeBlock) => {
   const { title, subtitle, imageUrl, body, buttonText, buttonLink, side } = props;
-
+  const [ref, inView] = useInView({ triggerOnce: true, rootMargin: '-150px' });
   return (
     <Grid
       gridColumnGap={{ base: 0, lg: 16, xl: 48 }}
@@ -27,7 +27,13 @@ export const HomeBlock = (props: IHomeBlock) => {
       gridTemplateRows={{ base: '0.33fr 1fr 0.1fr', lg: '0.5fr 1fr 0.1fr' }}
       gridTemplateAreas={templates[side]}
       textAlign={side === 'left' ? 'right' : 'left'}>
-      <Center boxSize="100%" gridArea="image" display={{ base: 'none', lg: 'flex' }}>
+      <Center
+        ref={ref}
+        boxSize="100%"
+        gridArea="image"
+        opacity={+inView}
+        transition="opacity 0.2s ease-in 0.1s"
+        display={{ base: 'none', lg: 'flex' }}>
         <Image boxSize="100%" src={imageUrl} />
       </Center>
       <Box gridArea="title">
