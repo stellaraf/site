843957674c1d770d700350194dbe90953c681a85
checktheroads <matt@allroads.io>
2021-01-09 23:41:33 -0700

    fix opengraph image urls

--- components/meta/SEO.tsx
+++ components/meta/SEO.tsx
@@ -1,25 +1,17 @@
 import { useRouter } from 'next/router';
 import { NextSeo } from 'next-seo';
 
-import type { NextSeoProps } from 'next-seo';
 import type { ISEO } from './types';
 
 export const SEO: React.FC<ISEO> = (props: ISEO) => {
   const { title, description, ...rest } = props;
   const { pathname } = useRouter();
 
-  // Ensure noindex/nofollow are set for preview site.
-  let indexFollow = {} as NextSeoProps;
-  if (process.env.GIT_BRANCH === 'develop') {
-    indexFollow = { nofollow: true, noindex: true };
-  }
-
   return (
     <NextSeo
       title={title}
       description={description}
-      openGraph={{ title, description, url: `/${pathname}` }}
-      {...indexFollow}
+      openGraph={{ title, description, url: pathname }}
       {...rest}
     />
   );
