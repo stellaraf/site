866213b8d972596f44588cd15a492dfc68322e27
thatmattlove <mtt.lov@gmail.com>
2021-08-04 12:26:02 -0700

    Migrate useKonami to zustand

--- hooks/useKonami.ts
+++ hooks/useKonami.ts
@@ -1,16 +1,33 @@
-import { createState, useState } from '@hookstate/core';
+import create from 'zustand';
 import { useMouseTrap } from './useMouseTrap';
 
-const konamiState = createState<boolean>(false);
+import type { SetState, GetState } from 'zustand';
 
-export const useKonami = (): boolean => {
-  const state = useState<boolean>(konamiState);
-  useMouseTrap(
-    'up up down down left right left right b a',
-    () => {
-      state.set(p => !p);
-    },
-    'keyup',
-  );
-  return state.value;
+type KonamiState = {
+  /**
+   * `true` if the combination has been pressed and is active, `false otherwise`.
+   */
+  ready: boolean;
+  /**
+   * Toggle the `ready` state to the opposite of its current value.
+   */
+  toggle: () => void;
 };
+
+const useStore = create<KonamiState>((set: SetState<KonamiState>, get: GetState<KonamiState>) => ({
+  ready: false,
+  toggle: () => {
+    const { ready } = get();
+    set({ ready: !ready });
+  },
+}));
+
+/**
+ * Custom hook for detecting when the konami combination is pressed from anywhere within the site.
+ */
+export function useKonami(): boolean {
+  const state = useStore(state => state.ready);
+  const toggle = useStore(state => state.toggle);
+  useMouseTrap('up up down down left right left right b a', toggle, 'keyup');
+  return state;
+}
