e0c4cb319ea0f0aa749d801e68bd149ca01e5f95
thatmattlove <mtt.lov@gmail.com>
2022-11-11 18:15:31 -0500

    remove unused components

--- src/components/contact-form/fields/types.ts
+++
@@ -1,8 +0,0 @@
-import type { SelectProps } from "~/components";
-import type { SelectOptionSingle } from "~/types";
-
-export interface SelectFieldProps
-  extends Omit<SelectProps<SelectOptionSingle>, "name" | "onSelect" | "options"> {
-  id: string;
-  opts: SelectOptionSingle[];
-}
