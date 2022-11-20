fac145b8275ebf875f9d3e18cd41a1e15132fb2a
thatmattlove <mtt.lov@gmail.com>
2022-11-20 10:13:15 -0500

    fix opengraph image handling

--- lib/next.ts
+++ lib/next.ts
@@ -10,3 +10,12 @@ export function isValidJsonRequest(request: NextApiRequest): request is ValidJso
   }
   return is(request.body) && typeof request.body === "object";
 }
+
+export function originFromEnv(env: NodeJS.ProcessEnv): URL {
+  let vercel_env = env.VERCEL_ENV ?? "development";
+  vercel_env = vercel_env.replace(/[^a-zA-Z0-9-]/g, "").toLowerCase();
+  if (!["production", "development"].includes(vercel_env)) {
+    return new URL(`https://${vercel_env}.stellar.tech`);
+  }
+  return new URL("https://stellar.tech");
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
-import App from "next/app";
-import type { AppContext, AppProps, AppInitialProps } from "next/app";
+import type { AppProps } from "next/app";
 
 import { DefaultSeo } from "next-seo";
 import queryString from "query-string";
@@ -9,36 +8,36 @@ import { Favicons } from "~/components";
 import { Provider } from "~/context";
 import { usePageTracking } from "~/hooks";
 import { SiteLayout } from "~/layouts";
-import { serializeUrl, parseCookie } from "~/lib";
+import { originFromEnv } from "~/lib";
 
 import type { PageProps } from "~/types";
 
-interface SiteProps extends AppProps<PageProps> {
-  requestUrl: ReturnType<typeof serializeUrl> | null;
-}
-
 const noIndexNoFollow = process.env.VERCEL_ENV !== "production";
 
-const Site = (props: SiteProps) => {
+const ORIGIN = originFromEnv(process.env);
+
+const Site = (props: AppProps<PageProps>) => {
   const {
     Component,
     pageProps,
-    requestUrl,
     router: { pathname },
   } = props;
   const { common, title: pageTitle, subtitle, footerTitle } = pageProps;
   const { config, theme, footerGroups, actions, docsGroups, twitterHandle } = common;
   const { organizationName, title, description } = config;
 
-  const { origin = "" } = requestUrl ?? {};
   const useFallback = pathname === "/" || typeof title !== "string";
 
   const imageUrl = useFallback
-    ? `${origin}/api/og/fallback`
-    : queryString.stringifyUrl({
-        url: `${origin}/api/og/page`,
-        query: { title, subtitle: description },
-      });
+    ? new URL("/api/og/fallback", ORIGIN)
+    : new URL(
+        queryString.stringifyUrl({
+          url: new URL("/api/og/page", ORIGIN).toString(),
+          query: { title, subtitle: description },
+        }),
+      );
+
+  const siteUrl = new URL(pathname, ORIGIN);
 
   const fnTitle = useTitleCase();
 
@@ -56,10 +55,10 @@ const Site = (props: SiteProps) => {
         additionalMetaTags={[{ name: "viewport", content: "width=device-width" }]}
         openGraph={{
           title,
-          url: origin + pathname,
           description,
           type: "website",
-          images: [{ url: imageUrl, width: 1200, height: 630, alt: title }],
+          url: siteUrl.toString(),
+          images: [{ url: imageUrl.toString(), width: 1200, height: 630, alt: title }],
         }}
       />
       <Favicons
@@ -76,25 +75,6 @@ const Site = (props: SiteProps) => {
       </SiteLayout>
     </Provider>
   );
-};
-
-Site.getInitialProps = async (appCtx: AppContext) => {
-  const initialProps: AppInitialProps<PageProps> = await App.getInitialProps(appCtx);
-  // const referrerHeader = appCtx.ctx.req?.headers.referer ?? "";
-  // appCtx.ctx.req?.headers.cookie
-  try {
-    const { __origin: origin } = parseCookie(appCtx.ctx.req?.headers.cookie ?? "");
-    if (typeof origin !== "string") {
-      throw new TypeError("origin header value must be a string");
-    }
-    const requestUrl = serializeUrl(new URL(origin));
-    return { requestUrl, ...initialProps };
-  } catch (error) {
-    console.group("Failed to parse referrer URL");
-    console.error(error);
-    console.groupEnd();
-  }
-  return { requestUrl: null, ...initialProps };
 };
 
 export default Site;
