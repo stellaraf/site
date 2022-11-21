48ea69988e0114cc5ef32412c0fc7b1dedb68a4a
thatmattlove <mtt.lov@gmail.com>
2022-11-21 16:36:13 -0500

    improve feature layout

--- src/components/content/content-feature-grid.tsx
+++ src/components/content/content-feature-grid.tsx
@@ -7,7 +7,34 @@ import type { ContentFeatureGridProps } from "./types";
 export const ContentFeatureGrid = (props: ContentFeatureGridProps) => {
   const { features, ...rest } = props;
   return (
-    <SimpleGrid columns={{ base: 1, lg: 2 }} spacing={16} my={16} {...rest}>
+    <SimpleGrid
+      columns={{ base: 2, lg: 4 }}
+      spacing={16}
+      my={16}
+      css={{
+        "& > *": {
+          gridColumn: "span 2",
+        },
+      }}
+      sx={
+        features.length % 2 == 0
+          ? undefined
+          : {
+              // CSS fuckery to center-align the last row which may not be full.
+              // See: https://css-irl.info/controlling-leftover-grid-items
+              "& :last-child:nth-of-type(2n - 1)": {
+                gridColumnEnd: { lg: -2 },
+              },
+              "& :nth-last-of-type(2):nth-of-type(2n + 1)": {
+                gridColumnEnd: { lg: 4 },
+              },
+              "& :last-child:nth-of-type(2n - 2)": {
+                gridColumnEnd: { lg: 5 },
+              },
+            }
+      }
+      {...rest}
+    >
       {features.map(feature => (
         <ContentFeature key={feature.title} {...feature} />
       ))}
