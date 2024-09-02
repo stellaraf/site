7e45372cc8eb2769a82310d4e50f88c89182dc67
thatmattlove <matt@stunninglyclear.com>
2024-09-02 17:12:21 -0400

    add session id cookie for mapbox

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,19 @@
 import { NextResponse } from "next/server";
+import { v4 as uuid4 } from "uuid";
 
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const response = NextResponse.next();
   response.cookies.set("__origin", request.nextUrl.origin);
+
+  let cookie = request.cookies.get("st-session-id");
+
+  if (typeof cookie === "undefined") {
+    response.cookies.set("st-session-id", uuid4());
+  }
+
+  cookie = response.cookies.get("st-session-id");
   return response;
 }
 
--- pages/api/address-search.ts
+++ pages/api/address-search.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { v4 as uuid4 } from "uuid";
 import { search } from "~/lib/server";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
@@ -13,8 +14,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (typeof req.query.locationType === "string" && req.query.locationType !== "") {
     locationType = req.query.locationType;
   }
+  const sessionId = req.cookies["st-session-id"] ?? uuid4();
+
   try {
-    const results = await search("857DE7DA-0FFC-4136-91F4-ECD681C5F645", q, locationType);
+    const results = await search(sessionId, q, locationType);
     return res.json(results);
   } catch (error) {
     console.error(error);
